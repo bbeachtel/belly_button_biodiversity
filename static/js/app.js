5109c32d3df38241311284fba5555071c016feca
Brent Beachtel <158010061+bbeachtel@users.noreply.github.com>
2024-05-09 16:28:41 -0500

    finished building metadata panel, added name data to populate dropdown selection

--- static/js/app.js
+++ static/js/app.js
@@ -1,24 +1,29 @@
 // Build the metadata panel
+const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
+
 function buildMetadata(sample) {
-  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-
-    // get the metadata field
-
+  d3.json(url).then((data) => {
+    // Get the metadata field
+    let metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+    let filteredMetadata = metadata.filter(entry => entry.id === sample);
 
     // Use d3 to select the panel with id of `#sample-metadata`
+    let samplePanel = d3.select('#sample-metadata');
 
+    // Use `.html("")` to clear any existing metadata
+    samplePanel.html("");
 
-    // Use `.html("") to clear any existing metadata
-
-
-    // Inside a loop, you will need to use d3 to append new
-    // tags for each key-value in the filtered metadata.
-
+    // Inside a loop, use d3 to append new tags for each key-value in the filtered metadata
+    filteredMetadata.forEach(entry => {
+      Object.entries(entry).forEach(([key, value]) => {
+        // Append the key-value pairs as <p> elements to the #sample-metadata panel
+        samplePanel.append('p').text(`${key}: ${value}`);
+      });
+    });
   });
-}
+};
 
 // function to build both charts
 function buildCharts(sample) {
@@ -56,21 +61,23 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
-
+    let names = data.names;
 
     // Use d3 to select the dropdown with id of `#selDataset`
-
+    let dropdown = d3.select('#selDataset')
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
-
+    for (let i=0, i<names.length, i++){
+      dropdown.append('option').attr('value', names[i]).text(names[i]);
+    }
 
     // Get the first sample from the list
-
+    let firstSample = names[0]
 
     // Build charts and metadata panel with the first sample
-
+    
   });
 }
 
